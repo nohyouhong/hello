e0bd8e39ee99f93196d5ef714fc8f693179f4b71
nohyouhong <106290849+nohyouhong@users.noreply.github.com>
2022-05-26 13:47:45 +0900

    Hello2 출력 추가

--- hello/src/hello/Java.java
+++ hello/src/hello/Java.java
@@ -2,6 +2,7 @@ package hello;
 
 public class Java {
 	public static void main(String[] args) {
-		System.out.println("Hello")
+		System.out.println("Hello");
+		System.out.println("Hello2");
 	}
 }
